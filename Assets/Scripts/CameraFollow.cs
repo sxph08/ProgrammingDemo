ddf23d737bb17b7f91f2d6e094e2fdb4c3456cc1
LauraTutor <lsanders@heartofyorkshire.ac.uk>
2025-10-08 16:28:23 +0100

    Update script

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,15 +3,39 @@ using UnityEngine.InputSystem;
 
 public class CameraFollow : MonoBehaviour
 {
-    public GameObject player;
-    private Vector3 offset;       
+
+    [SerializeField] private float YMin = -20.0f;
+    [SerializeField] private float YMax = 60.0f;
+
+    public Transform lookAt;
+
+    public Transform Player;
+
+    public float distance = 10.0f;
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+    public float sensivity = 4.0f;
+
+
+    // Start is called before the first frame update
     void Start()
     {
-        offset = transform.position - player.transform.position;
+
+
     }
 
+    // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        currentX += Input.GetAxis("Mouse X") * sensivity * Time.deltaTime;
+        currentY -= Input.GetAxis("Mouse Y") * sensivity * Time.deltaTime; // Invert Y to feel more natural
+
+        currentY = Mathf.Clamp(currentY, YMin, YMax);
+
+        Vector3 direction = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        transform.position = lookAt.position + rotation * direction;
+
+        transform.LookAt(lookAt.position);
     }
 }
